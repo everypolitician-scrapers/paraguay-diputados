2e1955bc28aa6a2a840b9f1202eb274194e1a151
Tony Bowden <tony@tmtm.com>
2018-09-18 14:59:53 +0100

    Rewrite for new layout

--- scraper.rb
+++ scraper.rb
@@ -1,27 +1,76 @@
 #!/bin/env ruby
-# encoding: utf-8
 # frozen_string_literal: true
 
 require 'pry'
 require 'scraped'
 require 'scraperwiki'
 
-require_rel 'lib'
-
 require 'open-uri/cached'
 OpenURI::Cache.cache_path = '.cache'
-# require 'scraped_page_archive/open-uri'
 
-def scrape(h)
-  url, klass = h.to_a.first
-  klass.new(response: Scraped::Request.new(url: url).response)
+def scraper(pair)
+  url, klass = pair.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response) rescue binding.pry
 end
 
-start = 'http://www.diputados.gov.py/ww1/?pagina=dip-listado'
-data = scrape(start => MembersPage).members.map do |mem|
-  mem.to_h.merge(scrape(mem.source => MemberPage).to_h)
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
+
+  field :member_urls do
+    noko.css('.listado_enlaces .card-title a/@href').map(&:text)
+  end
+
+  field :next_page do
+    noko.css('li.next a/@href').text
+  end
+end
+
+class MemberPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
+
+  field :id do
+    url.split('/').last
+  end
+
+  field :name do
+    noko.css('h3.page-title').text.tidy
+  end
+
+  field :area do
+    infobox.xpath('.//span[contains(text(),"Departamento:")]/following::text()').map(&:text).map(&:tidy).first
+  end
+
+  field :party do
+    infobox.xpath('.//span[contains(text(),"Partido:")]/following::text()').map(&:text).map(&:tidy).first
+  end
+
+  field :email do
+    # TODO: unprotect this
+  end
+
+  field :image do
+    noko.css('.ccm-layout-column-inner img/@src').map(&:text).first
+  end
+
+  private
+
+  def infobox
+    noko.css('.contenido_principal')
+  end
+end
+
+def member_urls_from(url)
+  return [] if url.to_s.empty?
+
+  page = scraper(url => MembersPage)
+  page.member_urls + member_urls_from(page.next_page)
+end
+
+start = 'http://www.diputados.gov.py/ww5/index.php/institucion/diputados-nacionales'
+data = member_urls_from(start).map do |url|
+  scraper(url => MemberPage).to_h
 end
 data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
 
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-ScraperWiki.save_sqlite(%i[id term], data)
+ScraperWiki.save_sqlite(%i[id], data)
